a034cd8111752419bf1fe990d592c5113cd06dfc
gord02 <gordon.hamilton1110@gmail.com>
2023-01-21 08:03:45 -0500

    added scheduler for backend logic of scraping

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
 
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
+
+# function to run web scrapping 
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=60)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
 
 
 # routes are controlled by functions, routes are called decorators and create paths 
